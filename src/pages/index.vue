a8829f37d1b1cca6c6661eaf3eda06189cf48e43
Vanisper <273266469@qq.com>
2026-08-20 20:55:01 +0800

    feat: 添加测试案例

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-definePage({
-  type: 'home',
-})
+const female = ref(false)
+function switchMale() {
+  female.value = !female.value
+}
 </script>
 
 <template>
-  <AppLogos />
-  <InputEntry />
+  <view @click="switchMale()">
+    <text v-if="!female" class="i-material-symbols-male-rounded text-[rgb(0,145,255)]">
+      👨 男
+    </text>
+    <text v-else class="i-material-symbols-female-rounded text-[rgb(255,55,95)]">
+      👩 女
+    </text>
+  </view>
 </template>
-
-<style></style>
